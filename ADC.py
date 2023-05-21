d3e41c6c17731e77df00a3a6fd123d038f9dcaf3
Diego Lopez <dl491945@gmail.com>
2023-05-20 20:15:20 -0600

    Quick Update
    I added a new button
    I changed function names

--- ADC.py
+++ ADC.py
@@ -3,19 +3,10 @@ from tkinter import *
 from tkinter import messagebox
 import pandas as pd
 
-def save_data():
-    first_name = entry.get()
-    last_name = entry2.get()
-    city = entry3.get()
-
-    data = pd.DataFrame({"Firstname": [first_name],
-                         "Lastname": [last_name],
-                         "City": [city]})
-    data.to_csv("data.csv", index=False)
-    messagebox.showinfo("The file has been created!")
-
-
-
+def new_file():
+    a = open("data.csv", "x")
+    a.close()
+    messagebox.showinfo("The file is created!")
 
 def apend_data():
     first_name = entry.get()
@@ -34,6 +25,10 @@ def delete_data():
     entry2.delete(0,END)
     entry3.delete(0,END)
 
+def delete_file():
+    os.remove("data.csv")
+    messagebox.showinfo("The file has been deleted")
+
 def focus_next_entry(event):
     event.widget.tk_focusNext().focus()
     return "break"
@@ -41,23 +36,19 @@ def focus_next_entry(event):
 window = Tk()
 window.geometry("575x375")
 window.title("Personal Detail")
-window.config(background="black")
+window.config(background="skyblue")
 
-title = Label(window, text="Fill up the empty fields",
-              font=("Arial", 25, "bold"),
-              fg="blue",
-              bg="black")
-
-
-label = Label(window, text="Firstname: ", font=("Arial", 18, "bold"), fg="green", bg="black")
-label2 = Label(window,text="Lastname: ", font=("Arial",18,"bold"),fg="green", bg="black")
-label3 = Label(window,text="State: ", font=("Arial",18,"bold"), fg="green", bg="black")
-entry = Entry(window, font=("Arial", 18), fg="black", bg="orange")
-entry2 = Entry(window, font=("Arial", 18), fg="black", bg="orange")
-entry3 = Entry(window, font=("Arial", 18), fg="black", bg="orange")
-button = Button(window, text="New", font=("arial",10,"bold"), fg="white", bg="blue",command=save_data)
-button2 = Button(window, text="Append", font=("arial",10,"bold"), fg="white", bg="blue",command=apend_data)
+label = Label(window, text="Firstname: ", font=("Arial", 18, "bold"), fg="green", bg="skyblue")
+label2 = Label(window,text="Lastname: ", font=("Arial",18,"bold"),fg="green", bg="skyblue")
+label3 = Label(window,text="State: ", font=("Arial",18,"bold"), fg="green", bg="skyblue")
+entry = Entry(window, font=("Arial", 18), fg="black", bg="white")
+entry2 = Entry(window, font=("Arial", 18), fg="black", bg="white")
+entry3 = Entry(window, font=("Arial", 18), fg="black", bg="white")
+button = Button(window, text="New", font=("arial",10,"bold"), fg="white", bg="blue",command=new_file)
+button2 = Button(window, text="Update", font=("arial",10,"bold"), fg="white", bg="blue",command=apend_data)
 button3 = Button(window, text="Delete", font=("arial",10,"bold"), fg="white", bg="blue",command=delete_data)
+button4 = Button(window, text="Delete File", font=("Arial",10,"bold"), fg="white", bg="blue", command=delete_file)
+
 label.grid(row=1, column=0, sticky="w", padx=10, pady=10)
 label2.grid(row=2, column=0, sticky="w", padx=10, pady=10)
 label3.grid(row=3, column=0, sticky="w", padx=10, pady=10)
@@ -67,7 +58,8 @@ entry3.grid(row=3, column=1, sticky="w", padx=10, pady=10)
 button.grid(row=4, column=1, sticky=W, padx=5, pady=5)
 button2.grid(row=5, column=1, sticky=W, padx=5, pady=5)
 button3.grid(row=6, column=1, sticky=W, padx=5, pady=5)
-title.grid(row=0, column=1)
+button4.grid(row=7, column=1, sticky=W, padx=5, pady=5)
+
 entry.bind("<Return>", focus_next_entry)
 entry2.bind("<Return>", focus_next_entry)
 entry3.bind("<Return>", focus_next_entry)
